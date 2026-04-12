abdb8f153fc6979696e4f97379d2c66fea1b82c4
ST10483489 <ST10483489@imconnect.edu.za>
2026-04-12 20:05:06 +0200

    Add files via upload

---
+++ Course.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.classpratical2;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class ClassPratical2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
